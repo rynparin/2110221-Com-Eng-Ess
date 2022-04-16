1a50c75c795d229f45a9db066fac27957988f42b
Parin Opas <ryn.parin@gmail.com>
2022-04-16 14:08:53 +0700

    Test popup button

---
+++ script.js
@@ -0,0 +1,14 @@
+function openForm() {
+	document.getElementById('myForm').style.visibility = 'visible';
+	console.log('click');
+}
+
+function closeForm() {
+	document.getElementById('myForm').style.visibility = 'hidden';
+}
+
+document.querySelector('.open-button').addEventListener('click', openForm);
+
+document
+	.querySelector('.popup__close-button')
+	.addEventListener('click', closeForm);

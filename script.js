3017d1acd2933afb1bc22797e205ecfefe79dda5
Akkharawat Burachokviwat <Akkharawatbct@gmail.com>
2022-04-20 01:18:08 +0700

    pop up when click number

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyDyo66tdyy-b-K6CWsU2ZvhbDISWVsW9gY',
-	authDomain: 'testfirebase-b134a.firebaseapp.com',
-	projectId: 'testfirebase-b134a',
-	storageBucket: 'testfirebase-b134a.appspot.com',
-	messagingSenderId: '183798996478',
-	appId: '1:183798996478:web:f994edb5885dcbc7e134af',
+  apiKey: "AIzaSyDyo66tdyy-b-K6CWsU2ZvhbDISWVsW9gY",
+  authDomain: "testfirebase-b134a.firebaseapp.com",
+  projectId: "testfirebase-b134a",
+  storageBucket: "testfirebase-b134a.appspot.com",
+  messagingSenderId: "183798996478",
+  appId: "1:183798996478:web:f994edb5885dcbc7e134af",
 };
 
 // Initialize Firebase
@@ -20,33 +20,40 @@ const app = initializeApp(firebaseConfig);
 
 // =======================================================
 import {
-	addDoc,
-	collection,
-	deleteDoc,
-	doc,
-	getDoc,
-	getDocs,
-	getFirestore,
-	updateDoc,
-} from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js';
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
 
 const db = getFirestore();
-const commentsRef = collection(db, 'comments');
-const questionsRef = collection(db, 'questions');
-const tagsRef = collection(db, 'tags');
-const usersRef = collection(db, 'users');
+const commentsRef = collection(db, "comments");
+const questionsRef = collection(db, "questions");
+const tagsRef = collection(db, "tags");
+const usersRef = collection(db, "users");
 
 function openForm() {
-	document.getElementById('myForm').style.visibility = 'visible';
-	console.log('click');
+  document.getElementById("myForm").style.visibility = "visible";
+  console.log("click");
 }
 
 function closeForm() {
-	document.getElementById('myForm').style.visibility = 'hidden';
+  document.getElementById("myForm").style.visibility = "hidden";
 }
 
-document.querySelector('.open-button').addEventListener('click', openForm);
+// document.querySelectorAll(".number").addEventListener("click", openForm);
+
+document.querySelectorAll(".number").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    //handle click
+	openForm()
+  });
+});
 
 document
-	.querySelector('.popup__close-button')
-	.addEventListener('click', closeForm);
+  .querySelector(".popup__close-button")
+  .addEventListener("click", closeForm);

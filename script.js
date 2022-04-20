efe61b30314ea44c2dcab8b906a0b7bb2a4d57db
Nalin Baipluthong <nalinbaipluthong@gmai.com>
2022-04-21 00:04:21 +0700

    creat function for add comment

--- script.js
+++ script.js
@@ -52,10 +52,28 @@ document
 	.addEventListener('click', closeForm);
 
 // =======================================================
-
+function addComment() {
+	var tag = document.createElement("div");
+	tag.className =  "ans";
+	const h3 = document.createElement("H3");
+	const commentText = document.getElementById("userAdd").value;
+	h3.innerHTML = commentText;
+	const button = document.createElement("button");
+	button.className = "like__btn";
+	button.innerHTML = '<img src="like.png" alt="like" class="like__img"/><p class="like__value">0</p>';
+	tag.appendChild(h3);
+	tag.appendChild(button);
+	document.getElementById("comment01").appendChild(tag);
+	console.log('add');
+}
 const likeButton = document.querySelector('.like__btn');
 
-const inputAddComment = document.querySelector('add__comment__input');
+const addButton = document.querySelector('.add__submit__btn');
+
+addButton.addEventListener('click', (e) =>{
+	e.preventDefault();
+	addComment();
+});
 
 likeButton.addEventListener('click', (e) => {
 	e.preventDefault();

53d63ef70044b3bc6905e9d372e3070f833d7139
Parin Opas <ryn.parin@gmail.com>
2022-04-22 21:46:07 +0700

    add login and question

--- script.js
+++ script.js
@@ -51,7 +51,138 @@ document
 	.querySelector('.popup__close-button')
 	.addEventListener('click', closeForm);
 
-// =======================================================
+//! Nutt
+
+var isLogin = false; // login status
+var inactiveStatus = false;
+
+var numberOfQuestion = {
+	FOREIGN_LANGUAGES: 1,
+	MATH: 1,
+	'P.E.': 1,
+	ART: 1,
+	HISTORY: 1,
+	SCIENCE: 1,
+	SOCIAL_STUDIES: 1,
+	GEOGRAPHY: 1,
+	OTHERS: 1,
+	TECHNOLOGY: 1,
+};
+
+// when the POST button is clicked, it calls this function to generate the question to the question box
+function generateQuestion() {
+	var question = document.getElementById('txt').value;
+	var category = document.getElementById('types').value;
+	var element = document.getElementById(category);
+	if (question != '' && category != 'CHOOSE CATEGORY') {
+		element.innerHTML +=
+			'<div class="parent"><div class="child"><div class="number">' +
+			numberOfQuestion[category] +
+			'</div></div><div class="child"><div class="message">' +
+			question +
+			'<div class="comments"><button type="button" class="num_comment">comments</button></div></div></div></div>';
+		numberOfQuestion[category] += 1;
+		var ID_comments = [];
+		addDoc(questionsRef, {
+			ID_comments,
+			category,
+			question,
+		});
+		// After the question is posted, set each field to default value.
+		document.getElementById('txt').value = '';
+		document.getElementById('types').value = 'CHOOSE CATEGORY';
+		return false;
+	}
+	alert(
+		'Please make sure that you have already typed the question and selected the category of the question.'
+	);
+	return false;
+}
+window.generateQuestion = generateQuestion;
+
+// when user is logging in, opacity = 100. Opacity = 0 otherwise.
+function changeStyle() {
+	var element = document.getElementById('mainElement');
+	if (isLogin) {
+		element.style.opacity = '100';
+		return false;
+	} else {
+		element.style.opacity = '0';
+		return false;
+	}
+}
+window.changeStyle = changeStyle;
+
+// onclick function for login button (checking username, lastname and the number of digit of student ID)
+async function validate() {
+	const firstname = document.getElementById('firstname').value;
+	const lastname = document.getElementById('lastname').value;
+	const studentId = document.getElementById('studentId').value;
+	if (
+		!isLogin &&
+		firstname != '' &&
+		lastname != '' &&
+		studentId.length == 10
+	) {
+		document.getElementById('welcome-msg').innerHTML =
+			'Welcome To STUDYTOGETHER';
+		isLogin = true;
+		changeStyle();
+		addDoc(usersRef, {
+			studentId,
+			firstname,
+			lastname,
+		});
+		if (!inactiveStatus) generateOldQuestion();
+		return false;
+	} else if (
+		!isLogin &&
+		firstname != '' &&
+		lastname != '' &&
+		studentId.length != 10
+	) {
+		alert('Please make sure that your student ID is 10-digit number.');
+		return false;
+	} else {
+		isLogin = false;
+		document.getElementById('welcome-msg').innerHTML =
+			'Please login before asking question !!!';
+		document.getElementById('firstname').value = '';
+		document.getElementById('lastname').value = '';
+		document.getElementById('studentId').value = '';
+		changeStyle();
+		inactiveStatus = true;
+		return false;
+	}
+	return false;
+}
+window.validate = validate;
+
+// generate old question which is posted before.
+async function generateOldQuestion() {
+	const items = await getDocs(questionsRef);
+	if (items) {
+		const oldquestion = items.docs.map((item) => ({
+			...item.data(),
+		}));
+		console.log(oldquestion);
+
+		for (let i = 0; i < oldquestion.length; i++) {
+			var element = document.getElementById(oldquestion[i].category);
+			element.innerHTML +=
+				'<div class="parent"><div class="child"><div class="number">' +
+				numberOfQuestion[oldquestion[i].category]++ +
+				'</div></div><div class="child"><div class="message">' +
+				oldquestion[i].question +
+				'<div class="comments"><button type="button" class="num_comment">comments</button></div></div></div></div>';
+			// console.log(oldquestion[i].type);
+			// console.log(oldquestion[i].question_text);
+		}
+	}
+}
+window.generateOldQuestion = generateOldQuestion;
+
+//!!!! =======================================================
 
 const addButton = document.querySelector('.add__submit__btn');
 

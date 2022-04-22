89756b8a3980b5ab25e94a095e6e00c8cc156f59
Akkharawat Burachokviwat <Akkharawatbct@gmail.com>
2022-04-22 15:01:51 +0700

    can get question and comments

--- script.js
+++ script.js
@@ -53,25 +53,113 @@ function closeForm() {
 
 // document.querySelectorAll(".number").addEventListener("click", openForm);
 
+const questions0 = await getDocs(questionsRef);
+
+const comments = await getDocs(commentsRef);
+const allComments = comments.docs.map((comment) => ({
+  docId: comment.id,
+  ...comment.data(),
+}));
+
+async function getCommentbyDocId(commentID){
+  // const docRef = doc(db, "comments", "commentID");
+  // const docSnap = await getDoc(docRef);
+  // const comment =  docSnap.data();
+  // return comment.toString();
+
+  for (let i = 0; i < allComments.length; i++) {
+    // console.log(allComments[i].docId);
+    // console.log(allComments[i].docId.toString());
+    // console.log(commentID.toString());
+    if (commentID.toString() == allComments[i].docId.toString()){
+      console.log(allComments[i].comment);
+      return allComments[i].comment;
+    }
+  }
+
+}
+
 document.querySelectorAll(".message").forEach((item) => {
   item.addEventListener("click", (event) => {
-    //handle click
-  var question = item.innerText;
-  console.log(question);
-  const q = query(questionsRef, where("question", "==", question));
 
-	openForm();
+    //handle click
+    var question__on__btn = item.innerText;
+    console.log(question__on__btn);
+    // const q = query(questionsRef, where("question", "==", question));
+    // console.log(questions);
+    const allQuestions = questions0.docs.map((question) => ({
+      docId: question.id,
+      ...question.data(),
+    }));
+
+    //async function getQuestion(allquestion) {
+      for (let i = 0; i < allQuestions.length; i++) {
+        // console.log(allQuestions[i]);
+        if (question__on__btn == allQuestions[i].question){
+          console.log("YES");
+          
+          for (let j = 0;j < allQuestions[i].ID_comments.length;++j){
+            var commentID = allQuestions[i].ID_comments[j];
+            commentID = commentID.trim();
+            // console.log(commentID);
+            
+            for (let i = 0; i < allComments.length; i++) {
+              // console.log(allComments[i].docId);
+              // console.log(allComments[i].docId.toString());
+              // console.log(commentID.toString());
+              if (commentID.toString() == allComments[i].docId.toString()){
+                console.log(allComments[i].comment);
+              }
+            }
+          }
+
+        }
+
+      }
+    //}
+    openForm();
   });
 });
 
 
-if(document.getElementsByClassName('message').clicked == true)
-{
-  //  alert("button was clicked");
-  console.log("clickkkk");
+
+async function getComments(question, comRefs) {
+  console.log("getComments");
+  // console.log(comRefs);
+
+  const comments = [];
+
+  for (let i = 0; i < comRefs.length; i++) {
+    const comment = await doc(db, `comments/${comRefs[i]}`);
+    // console.log(comment);
+    
+    let commentInstance = await getDoc(comment);
+    // console.log(commentInstance);
+
+    commentInstance = commentInstance.data();
+    // console.log(commentInstance);
+
+    comments.push(commentInstance);
+  }
+
+  console.log(comments);
 }
 
+// test get comment
+const questions = await getDocs(questionsRef);
+// console.log(questions);
+const newQuestions = questions.docs.map((question) => ({
+  docId: question.id,
+  ...question.data(),
+}));
+// console.log(newQuestions);
+getComments("asd", newQuestions[0].ID_comments);
 
+// if(document.getElementsByClassName('message').clicked == true)
+// {
+//   //  alert("button was clicked");
+//   console.log("clickkkk");
+// }
 
 document
   .querySelector(".popup__close-button")
